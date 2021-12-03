7461e69c81e6ef5368ac296006f132e336a907c0
jinwoo <gkwlsdn11@gmail.com>
2021-12-03 13:50:46 +0900

    lbvmss commit

--- test/13_lnvmss.tf
+++ test/13_lnvmss.tf
@@ -1,11 +1,13 @@
 resource "azurerm_linux_virtual_machine_scale_set" "jinwoo-vmss" {
-  name = "jinwoo-vmss"
+  name                = "jinwoo-vmss"
   resource_group_name = azurerm_resource_group.jinwoo-rg.name
-  location = azurerm_resource_group.jinwoo-rg.location
-  sku = "Standard_F2"
-  instances = 4
-  admin_username = "haha"
-  admin_password = "It12345678910!"
+  location            = azurerm_resource_group.jinwoo-rg.location
+  sku                 = "Standard_DS1_v2"
+  instances           = 2
+  admin_username      = "haha"
+  admin_password      = "It12345678910!"
+
+  #  health_probe_id = azurerm_lb_probe.jinwoo-lb-probe.id
 
   disable_password_authentication = false
 
@@ -18,16 +20,16 @@ resource "azurerm_linux_virtual_machine_scale_set" "jinwoo-vmss" {
 
   os_disk {
     storage_account_type = "Standard_LRS"
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
   }
   network_interface {
-    name = "jwh-vmss-nif"
+    name    = "jwh-vmss-nif"
     primary = true
 
     ip_configuration {
-      name = "jinwoo-ip-conf"
-      primary = true
-      subnet_id = azurerm_subnet.jinwoo-subnet-02.id
+      name                                   = "vmss_ip_conf"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.jinwoo-subnet-01.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.jinwoo-lb-back.id]
     }
   }
